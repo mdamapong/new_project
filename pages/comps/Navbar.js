b313564790cc8c33df7eef66ce08df24c8e3d08d
mickey <damapongm@gmail.com>
2022-04-17 09:33:32 -0700

    setting up nav style

---
+++ pages/comps/Navbar.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+const Navbar = () => {
+	return (
+		<nav>
+			<div className='logo'>
+				<h1>My List</h1>
+			</div>
+			<Link href='/'>
+				<a>Home</a>
+			</Link>
+			<Link href='about'>
+				<a>About</a>
+			</Link>
+			<Link href='/list'>
+				<a>My Listing</a>
+			</Link>
+		</nav>
+	);
+};
+
+export default Navbar;
